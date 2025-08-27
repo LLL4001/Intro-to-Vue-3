a34fc391e1a6a493e8ec71d938db7883c074bdb7
LLL4001 <129660644+LLL4001@users.noreply.github.com>
2025-08-27 18:16:08 +0200

    l5 chalenge complete

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['xxl', 'xl', 'l', 'm', 's'],
             variants: [
                 { id: 2234, color: 'green'},
                 { id: 2235, color: 'blue'},
             ]
+            
         }
     }
 })
